460eb559f1b74bc86acaa007918a51cb2ced13a9
Ahmed Zain Al-Hamawi <78156960+azain95@users.noreply.github.com>
2021-02-02 19:57:07 +0300

    Solved the Conditionals task
    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
 // Define the numbers and the operation here
+let shawerma = 8
+let secondnumber = 5
+let mult = "*"
 
 // Write if-statements here
+
+if (mult === "+")  {  console.log(shawerma+secondnumber);  
+
+} else if (mult === "*") {console.log(shawerma*secondnumber)
+    
+}else if (mult === "/") {console.log(shawerma/secondnumber)
+    
+}else if (mult === "-") {console.log(shawerma-secondnumber)
+    
+}
